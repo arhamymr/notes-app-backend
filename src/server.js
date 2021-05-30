44308a72cc7f8afdd7d70b03685ac206f1aeef94
arhamymr <arhamymr@gmail.com>
2021-05-30 15:36:44 +0800

    update ip

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '3.0.100.101',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.18.201',
     routes: {
       cors: {
         origin: ['*'],
