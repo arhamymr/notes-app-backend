91a62908c4e72452e000283abc267af67d127e19
arhamymr <arhamymr@gmail.com>
2021-05-30 17:27:15 +0800

    update host

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '3.0.100.101',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : 'ec2-3-0-100-101.ap-southeast-1.compute.amazonaws.com',
     routes: {
       cors: {
         origin: ['*'],
