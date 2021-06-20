19645f4275dc4e6ee6d55f739ae5303549778d3a
arhamymr <arhamymr@gmail.com>
2021-06-20 13:19:33 +0800

    update

--- src/handler/youtubeHandler.js
+++ src/handler/youtubeHandler.js
@@ -1,34 +1,13 @@
-const { nanoid } = require('nanoid');
 const youtubedl = require('youtube-dl-exec')
 
-const downloadHandler = async (request, h) => {
+const downloadHandler = async (request) => {
   const { url } = request.params;
-  let yt = null
   try {
-    await youtubedl(`www.youtube.com/watch?v=${url}`, {
-    dumpSingleJson: true,
-    noWarnings: true,
-    noCallHome: true,
-    noCheckCertificate: true,
-    preferFreeFormats: true,
-    youtubeSkipDashManifest: true,
-    referer: `www.youtube.com/watch?v=${url}`
-    })
-    .then(output => {
-      yt = output
-    });
-
-    const response = h.response({
-      status: 'success',
-      message: 'Ok boy',
-      data: yt,
-    });
-
+    return await youtubedl(`www.youtube.com/watch?v=${url}`)
+    .then(output => output);
   } catch (error) {
     console.log(new Error(error));
   }
-  
-  return yt;
 }
 
 module.exports = {
